1b2e5c7eccbdd574706b24066e3380a83fdfbcba
AlbertoPonce <38371708+ChessePrince@users.noreply.github.com>
2022-08-05 07:02:59 -0400

    Music and effect sliders

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
+    //References
+    public Transform trnsGun;
+    public Transform trnsGunTip;
+    public SpriteRenderer sprRndDude;
+    public SpriteRenderer sprRndGun;
     //Movement
     public float movementSpeed;
     private Vector2 inputVector;
+
+    private Vector2 mousePos;
+    private Vector2 mouseWorldPos;
     void Update()
     {
         Movement();
+        RotateGun();
 
+        FlipSprites();
+        //Shoot();
     }
 
     void Movement()
@@ -21,6 +32,42 @@ public class TopDownMovement : MonoBehaviour
         if (inputVector != Vector2.zero)
         {
             transform.position += (Vector3)inputVector.normalized * movementSpeed * Time.deltaTime;
+        }
+    }
+    void RotateGun()
+    {
+        mousePos = Input.mousePosition;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(trnsGun.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        trnsGun.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    void Shoot()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //GameObject insantiatedMelon = Instantiate(melon, trnsGunTip.position, Quaternion.identity);
+            //insantiatedMelon.GetComponent<FakeHeightObject>().Initialize(trnsGun.right * Random.Range(groundDispenseVelocity.x, groundDispenseVelocity.y), Random.Range(verticalDispenseVelocity.x, verticalDispenseVelocity.y));
+        }
+    }
+    void FlipSprites()
+    {
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mouseWorldPos.x > transform.position.x)
+        {
+            sprRndDude.flipX = false;
+            sprRndGun.flipY = false;
+        }
+        else
+        {
+            sprRndDude.flipX = true;
+            sprRndGun.flipY = true;
         }
     }
 }
